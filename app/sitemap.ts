74754163ea63b5d1e3baa05d3089e3373b89a97d
srajesh1330 <sprajeshofficial@gmail.com>
2026-09-02 14:54:05 +0530

    Add New Year page to sitemap

--- app/sitemap.ts
+++ app/sitemap.ts
@@ -19,6 +19,12 @@ export default function sitemap(): MetadataRoute.Sitemap {
       priority: 0.9,
     },
     {
+      url: `${SITE_URL}/fireworks/new-year`,
+      lastModified,
+      changeFrequency: "monthly",
+      priority: 0.9,
+    },
+    {
       url: `${SITE_URL}/about`,
       lastModified,
       changeFrequency: "monthly",
